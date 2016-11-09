a497f62a648ecc303444a7e10fe80150b8054228
Victor <papadoginc@gmail.com>
2016-11-09 12:20:14 +0300

    Task completed

--- src/index.js
+++ src/index.js
@@ -9,6 +9,27 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/task2b',(req,res)=>{
+    let result = "";
+    let strArr = [];
+    strArr = req.query.fullname.split(" ");
+    
+    if(strArr.length === 1 && strArr[0]===''){
+         result = "Invalid fullname";
+    } else if(strArr.length === 1){
+        result = strArr[0];
+    } else if(strArr.length === 2){
+        result = strArr[1]+" " + strArr[0][0]+".";
+    } else if(strArr.length === 3){
+        result = strArr[2]+" " + strArr[0][0]+". "+ strArr[1][0]+".";
+    } else {
+        result = "Invalid fullname";
+    }
+    
+   res.send(result); 
+});
+
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
